621bffb55a97dfe13eec0d22f6cef2c91b8185d0
sougata <skar7340@gmail.com>
2023-03-11 00:03:41 +0530

    minor changes

--- app/src/main/java/com/example/testing/recyclerview/olivia/activities/CallActivity.kt
+++ app/src/main/java/com/example/testing/recyclerview/olivia/activities/CallActivity.kt
@@ -60,7 +60,7 @@ class CallActivity : AppCompatActivity() {
             callJavaScriptFunction("javascript:toggleAudio(\"$isAudio\")")
 
 //            if (isAudio) {
-//                binding.micBtn.setImageResource(R.drawable.numute_n.png)
+//                binding.micBtn.setImageResource(R.drawable.numute_n)
 //            } else {
 //                binding.micBtn.setImageResource(R.drawable.mute_p)
 //            }
